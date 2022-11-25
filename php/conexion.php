be969c06c07e03de2b47b2247dd757c3121b3e7e
wdurrea <duvan9760@hotmail.com>
2022-11-25 09:09:24 -0500

    Se protegen datos de acceso a la BD

--- php/conexion.php
+++ php/conexion.php
@@ -1,13 +1,10 @@
 <?php
 
-function conectar(){
-    $user="admin";
-    $passwd="Saturno2309#";
-    $server="freivoosdb.cdrmiw6khp29.us-east-1.rds.amazonaws.com";
-    $port="3306";
-    $db="freivoosdb";
+require_once('/config/config.php');
+
+function conectar(){;
     //$con= mysqli_connect($server,$user,$passwd) or die("Error de conexión a la Base de Datos".mysqli_error($con));
-    $con= new mysqli($server,$user,$passwd,$db,$port) or die("Error de conexión a la Base de Datos".mysqli_error($con));
+    $con= new mysqli($servidor,$usuario,$pass,$bbdd,$port) or die("Error de conexión a la Base de Datos".mysqli_error($con));
     //mysqli_query($db,$con);
     mysqli_query($con, "select * from t_users" );
 
---
+++ php/config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+//Datos de configuración de la BD.
+
+$servidor = "freivoosdb.cdrmiw6khp29.us-east-1.rds.amazonaws.com";
+$usuario = "admin";
+$pass = "Saturno2309#";
+$bbdd = "freivoosdb";
+$port = "3306"
+?>
