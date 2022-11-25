d0da37be6bcae79776d28b7cc48a4eaf81dfc4be
wdurrea <duvan9760@hotmail.com>
2022-11-25 09:48:55 -0500

    Se agrega comentario

--- php/conexion.php
+++ php/conexion.php
@@ -3,7 +3,7 @@
 require_once('/config/config.php');
 
 function conectar(){;
-    //$con= mysqli_connect($server,$user,$passwd) or die("Error de conexión a la Base de Datos".mysqli_error($con));
+    //Recuerde ingresar contraseña en el archivo /config/config.php
     $con= new mysqli($servidor,$usuario,$pass,$bbdd,$port) or die("Error de conexión a la Base de Datos".mysqli_error($con));
     //mysqli_query($db,$con);
     mysqli_query($con, "select * from t_users" );
